98e2c020fd9d70c8e36b8133b4169a4bf036c5d6
bbaynat <bbaynatalia@gmail.com>
2025-03-02 21:49:12 +0500

    Solution

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 import org.postgresql.Driver;
 
@@ -9,18 +11,23 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        // реализуйте алгоритм здесь
         Class<Driver> driverClass = Driver.class;
-        String createTable = """
-                
-                
-                
-                """;
+
         try (Connection connection = Util.open();
              var statement = connection.createStatement()) {
-            System.out.println(connection.getTransactionIsolation());
-            boolean executeResult = statement.execute(createTable);
-            System.out.println(executeResult);
+            System.out.println("Уровень изолированности "+connection.getTransactionIsolation());
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ann", "Brown", (byte) 18);
+        userService.saveUser("Ben", "Green", (byte) 28);
+        userService.saveUser("Jack", "Black", (byte) 34);
+        userService.saveUser("George", "Cooper", (byte) 56);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
